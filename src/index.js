996c8113c02d49eeb8a87f7bc22621b258778904
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-05-20 14:15:47 +0530

    remove th from headings selector

--- src/index.js
+++ src/index.js
@@ -76,7 +76,7 @@ module.exports = plugin.withOptions(
       let options = { className, prefix }
 
       for (let [name, ...selectors] of [
-        ['headings', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'th'],
+        ['headings', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
         ['h1'],
         ['h2'],
         ['h3'],
